42e869a5dc42701bbde5923c06e94dae5681159f
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-07-02 18:27:26 -0300

    :sparkles: feat(routes.js): Routes configuration with middleware(muler)

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+const routes = require("express").Router();
+const multer = require("multer");
+const multerConfig = require("./config/multer");
+routes.post("/posts", multer(multerConfig).single("file"), (req, res) => {
+  return res.json({ hello: "world" });
+});
+module.exports = routes;
