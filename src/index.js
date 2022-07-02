fe93e6824d3d9f8f21f42fa7fde7b8767f9883a4
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-07-02 18:40:41 -0300

    :sparkles: feat(index.js): Add mongodb connection to the server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 //log's lib
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+
 const app = express();
+
+// Database setup
+const url = "localhost";
+const port = 27017;
+const dbname = "upload";
+mongoose.connect(`mongodb://${url}:${port}/${dbname}`, {
+  useNewUrlParser: true,
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(require("./routes"));
 
-app.listen(3000, console.log("Server's running"));
+app.listen(3000, () => {
+  console.log("Server's running");
+});
