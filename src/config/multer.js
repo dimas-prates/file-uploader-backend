f3b333c59f0ad51cf80619271c6b77a517b28cc8
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-07-08 11:41:22 -0300

    :sparkles: feat(multer): Still trying to figure out how to make work with IBM cloud

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,9 +1,24 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 const crypto = require("crypto");
 
-const IBM = require('ibm-cos-sdk')
-// const multerS3 = require("multer-s3");
+// const aws = require("ibm-cos-sdk");
+// const aws = require("aws-sdk");
+// const multerS3 = require("multer-s3")
+// var ep = new aws.Endpoint('https://s3.us-south.cloud-object-storage.appdomain.cloud');
+// var s3 = new aws.S3({endpoint: ep, region: 'us-south'});
+
+// var config = {
+//   endpoint: "https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints",
+//   apiKeyId: "ouXqcLMnpIo3o61-IhQqLtSGvvrbERj9zxS_yPMskHTw",
+//   serviceInstanceId:
+//     "crn:v1:bluemix:public:cloud-object-storage:global:a/9335c3383bd3418682a98e1973767233:2aee272f-a4ec-497b-ad21-e126ef93dfd6::",
+//   signatureVersion: "iam",
+// };
+
+// var cos = new aws.S3(config);
+
 const storageType = {
   local: multer.diskStorage({
     destination: (req, file, cb) => {
@@ -19,21 +34,32 @@ const storageType = {
       });
     },
   }),
-  //   s3: multerS3({
-  //     s3: new IBM.S3(),
-  //     bucket: "uploadexample2",
-  //     contentType: multerS3.AUTO_CONTENT_TYPE,
-  //     acl: "public-read",
-  //     key: (req, file, cb) => {
-  //       crypto.randomBytes(16, (err, hash) => {
-  //         if (err) {
-  //           cb(err);
-  //         }
-  //         const fileName = `${hash.toString("hex")}-${file.originalname}`;
-  //         cb(null, fileName);
-  //       });
-  //     },
-  //   }),
+
+    
+  // s3: multerS3({
+  //   s3: new aws.S3({endpoint: ep, region: 'us-south'}),
+  //   bucket: "uploadexample2",
+  //   key: (req, file, cb) => {
+  //     crypto.randomBytes(16, (err, hash) => {
+  //       if (err) {
+  //         cb(err);
+  //       }
+  //       const fileName = `${hash.toString("hex")}-${file.originalname}`;
+  //       cb(null, fileName);
+  //     });
+  //   },
+  // }),
+
+//   s3:  multer({
+//     storage: multerS3({
+//         s3: s3,
+//         bucket: myBucket,
+//         key: function (req, file, cb) {
+//             cb(null, file.originalname);
+//             console.log(file);
+//         }
+//     })
+// })
 };
 
 module.exports = {
